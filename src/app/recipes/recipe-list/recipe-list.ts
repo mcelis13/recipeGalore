45a56569aaa6a74d1ea633801e4464d92349f74f
Madeline Celis <celiscb@vcu.edu>
2020-02-21 11:55:26 -0500

    adding recipe model, making header title change and adding html for recipe list and details

--- src/app/recipes/recipe-list/recipe-list.ts
+++ src/app/recipes/recipe-list/recipe-list.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from '../recipe.model';
 @Component ({
     selector: 'recipe-list',
     templateUrl: '../recipe-list/recipe-list.html',
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
 })
 
 export class RecipeList {
-    
+    recipes: Recipe[] = [
+        new Recipe('Stroganoff', 'A delicious recipe of egg noodles and beef with creamy mushroom sauce.', 'https://www.gimmesomeoven.com/wp-content/uploads/2014/02/30-Minute-Beef-Stroganoff-Recipe-1.jpg'),
+        new Recipe('Stroganoff', 'A delicious recipe of egg noodles and beef with creamy mushroom sauce.', 'https://www.gimmesomeoven.com/wp-content/uploads/2014/02/30-Minute-Beef-Stroganoff-Recipe-1.jpg'),
+    ];
 }
